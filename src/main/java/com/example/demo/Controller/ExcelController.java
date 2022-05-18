f54b362bdfe4e61b9cec929c436c065f60724c09
Antony-Asish <tonyja1331@gmail.com>
2022-05-17 23:05:51 -0700

    Excel Report

--- src/main/java/com/example/demo/Controller/ExcelController.java
+++ src/main/java/com/example/demo/Controller/ExcelController.java
@@ -1,11 +1,19 @@
 package com.example.demo.Controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Service.CandidateService;
+import com.example.demo.Service.ExcelService;
+import com.example.demo.model.CandidateDetail;
+import com.example.demo.repository.CandidateRepository;
 
 @RestController
 @CrossOrigin
@@ -13,11 +21,21 @@ public class ExcelController {
 
 	@Autowired
 	CandidateService candidateService;
+	
+	@Autowired
+	CandidateRepository candidateRepo;
 		
 	//   GIVE LIST OF DETAIL BY PAGE 
 	@GetMapping("getExcelReport")
-	public void excelImport()
+	public void exportToExcel(HttpServletResponse response) throws IOException
 	{
+		response.setContentType("application/octet-stream");
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment;filename=CandidateDetails.xlsx";
+		
+		response.setHeader(headerKey, headerValue);
+		List<CandidateDetail> listCandidate=candidateRepo.findAll();
+		ExcelService excelExporter=new ExcelService(listCandidate);
+		excelExporter.export(response);
 	}
-
 }
--- src/main/java/com/example/demo/Service/ExcelService.java
+++ src/main/java/com/example/demo/Service/ExcelService.java
@@ -1,9 +1,17 @@
 package com.example.demo.Service;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -12,17 +20,88 @@ import com.example.demo.model.CandidateDetail;
 public class ExcelService {
 	private XSSFWorkbook workBook;
 	private XSSFSheet sheet;
-	private List<CandidateDetail> candidateDetail;
+	private List<CandidateDetail> litOfCandidate;
 	
-	public ExcelService(List<CandidateDetail> candidateDetail)
+	public ExcelService(List<CandidateDetail> litOfCandidate)
 	{
-		this.candidateDetail=candidateDetail;
+		this.litOfCandidate=litOfCandidate;
 		workBook=new XSSFWorkbook();	
 	}
 	
 	public void createCell(Row row,int columnCount,Object value,CellStyle style)
 	{
 		sheet.autoSizeColumn(columnCount);
-		//Cell
+		Cell cell=row.createCell(columnCount);
+		if(value instanceof Long)
+		{
+			cell.setCellValue((Long) value);
+		}
+		else if(value instanceof Integer)
+		{
+			cell.setCellValue((Integer) value);
+		}
+		else if(value instanceof Boolean)
+		{
+			cell.setCellValue((Boolean) value);
+		}
+		else
+		{
+			cell.setCellValue((String) value);
+		}
+		cell.setCellStyle(style);
+	}
+	
+	public void writeHeaderLine() {
+		sheet=workBook.createSheet("CandidateDetail");
+		Row row=sheet.createRow(0);
+		CellStyle style=workBook.createCellStyle();
+		XSSFFont font=workBook.createFont();
+		font.setBold(true);
+		font.setFontHeight(20);
+		style.setFont(font);
+		style.setAlignment(HorizontalAlignment.CENTER);
+		createCell(row,0,"Candidate Detail",style);
+		sheet.addMergedRegion(new CellRangeAddress(0,0,0,4));
+		font.setFontHeightInPoints((short)(10));
+		
+		row=sheet.createRow(1);
+		font.setBold(true);
+		font.setFontHeight(16);
+		style.setFont(font);
+		createCell(row,0,"Id",style);
+		createCell(row,1,"Name",style);
+		createCell(row,2,"Email",style);
+		createCell(row,3,"Gender",style);
+		createCell(row,4,"Phone",style);
+	}
+	private void writeDataLines()
+	{
+		int rowCount=2;
+		 CellStyle style=workBook.createCellStyle();
+		 XSSFFont font=workBook.createFont();
+		 font.setFontHeight(14);
+		 style.setFont(font);
+		 
+		 for(CandidateDetail candidate :litOfCandidate )
+		 {
+			 Row row=sheet.createRow(rowCount++);
+			 int columnCount=0;
+			 createCell(row,columnCount++,candidate.getId(),style);
+			 createCell(row,columnCount++,candidate.getFirstName(),style);
+			 createCell(row,columnCount++,candidate.getEmail(),style);
+			 createCell(row,columnCount++,candidate.getGender(),style);
+			 createCell(row,columnCount++,candidate.getPhone(),style);
+		 }
+	}
+	
+	public void export(HttpServletResponse response) throws IOException
+	{
+		writeHeaderLine();
+		writeDataLines();
+		
+		ServletOutputStream outPutStream=response.getOutputStream();
+		workBook.write(outPutStream);
+		workBook.close();
+		outPutStream.close();
 	}
 }
